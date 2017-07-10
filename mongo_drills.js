a22efecd58bfaa170809ef2a6d9617982d1ca090
daviddinnison <david.g.dinnison@gmail.com>
2017-07-10 16:54:27 -0400

    finished drill

--- mongo_drills.js
+++ mongo_drills.js
@@ -16,23 +16,67 @@ db.restaurants.find( { $and:
         },
         {
             "borough" : { $ne: "Missing" }
-        }, {
-            "grades.length" : { $gte: 1 }
-            }
+        }, 
+        {
+            "grades" : { $ne: [] }
+            },
+        {
+            "grades.grade" : { $ne: 'Not Yet Graded'}
+        },
+        {
+            "grades.score" : { $gte: 0}
+        }
     ]   
 } ).count();
 
-db.restaurants.find("grades.length" : { $gte: 1} ).count();
+
+
 
 
 //2
 
 // How many restaurants were filtered out?
 
+    //original total - 25359
+db.restaurants.find({}).count();
+
+    //filtered total - 24030
+db.restaurants.find( { $and: 
+    [ 
+        {
+            "cuisine" : { $ne: "Not Listed/Not Applicable" } 
+        },
+        {
+            "borough" : { $ne: "Missing" }
+        }, 
+        {
+            "grades" : { $ne: [] }
+            },
+        {
+            "grades.grade" : { $ne: 'Not Yet Graded'}
+        },
+        {
+            "grades.score" : { $gte: 0}
+        }
+    ]   
+} ).count();
 
 //3
 
+//mongo ds153652.mlab.com:53652/sandbox -u devdev -p devdev1
 // Ignoring your filter (i.e. querying all of the restaurants):
 // Create a list of all of the cuisines offered by the restaurants, with no duplicates
+db.runCommand({
+    distinct: "restaurants", key: "cuisine"
+});
+    
+
 // Find the first five restaurants with the lowest positive score in one of their gradings.
+db.restaurants.find({
+    "grades.score" : { $eq: 2}
+}).limit(5).pretty();
+
 // Find the restaurants which have only been graded A.
+db.restaurants.find({
+    "grades": {"$not": {"$elemMatch":{"grade":{$nin:["A"]}}}}
+}).pretty();
